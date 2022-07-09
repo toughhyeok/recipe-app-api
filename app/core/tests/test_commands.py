a620037557a4013544cca7a7e569e733d4512c30
junhyeok <toughhyeok96@gmail.com>
2022-07-10 00:41:55 +0900

    fix typo

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,7 +11,7 @@ from django.test import SimpleTestCase
 
 
 @patch("core.management.commands.wait_for_db.Command.check")
-class CommandTest(SimpleTestCase):
+class CommandTests(SimpleTestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
@@ -24,7 +24,7 @@ class CommandTest(SimpleTestCase):
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for databse when getting OperationalError"""
+        """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
 
