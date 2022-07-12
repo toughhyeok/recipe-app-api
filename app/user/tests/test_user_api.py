32df6a42445c46041cee251e626190e82689158e
junhyeok <toughhyeok96@gmail.com>
2022-07-12 23:38:25 +0900

    fix indentaion in user serializer

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -50,7 +50,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
-        """Test an error is returned if passwod less than 5 chars."""
+        """Test an error is returned if password less than 5 chars."""
         payload = {
             'email': 'test@example.com',
             'password': 'pw',
